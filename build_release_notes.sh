14c5cb834243ff4c93131ed4dc9a811cfbc206c4
Marc Vieira-Cardinal <marc.cardinal@gmail.com>
2018-12-24 15:41:46 -0500

    Removed leading path from files in release notes

--- build_release_notes.sh
+++ build_release_notes.sh
@@ -4,13 +4,15 @@ for f in mods/*
 do
     echo "Working on ${f}"
 
+    FN="${f##*/}"
+
     VERSION=`unzip -p $f modDesc.xml | xmlstarlet sel -t -m "/modDesc" -v version`
     if [ $? -ne 0 ]; then
         echo "Received an error while trying to determine version"
         VERSION="???"
     fi
 
-    NOTES=`grep -Fxq "## ${f##*/}" NOTES.md && echo "*"`
+    NOTES=`grep -Fxq "## ${FN}" NOTES.md && echo "*"`
 
-    echo " * ${f} (v${VERSION})${NOTES}" >> $1
+    echo " * ${FN} (v${VERSION})${NOTES}" >> $1
 done
